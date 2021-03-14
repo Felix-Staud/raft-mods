3a4114cf3a6bca9ab44a64bca09f83f304923f3d
Felix-Puetz <FPuetz@gmx.net>
2021-03-14 15:27:31 +0100

    Release v3.0
    - add optional modifier configuration (thanks to Aidanamite)
    - major code-cleanup

--- almost-no-durability/README.md
+++ almost-no-durability/README.md
@@ -8,10 +8,18 @@
 # Info
 - ### Multiplayer approved
   - _We tested this with two persons, both having the mod running, and it seemed to work fine_
-- feel free to open an [issue](https://github.com/Felix-Puetz/raft-mods/issues) if you find bugs
+- ### Configurable [optional. requires dependency mod !!!]
+  - **To be able to configure your durability you have to have [extra-settings-api](https://www.raftmodding.com/mods/extra-settings-api) mod installed !!!**
+  - Configuration Options range **from 2x to 10x** or default back to inifinte
+  - *supported since v3.0*
+
+-------------
+
+> Feel free to open an [issue](https://github.com/Felix-Puetz/raft-mods/issues) if you find bugs or have feature requests
+
+-------------
 
 # Thanks to
 - "Akaren" for testing if it works in multiplayer 
-- "Aidanamite" for the coding support
-- [YourBalance](https://www.raftmodding.com/mods/yourbalance) (by Akitake) for code-insight so I was able to scratch together this mod
+- [Aidanamite](https://www.raftmodding.com/user/Aidanamite) for the coding support & the [extra-settings-api](https://www.raftmodding.com/mods/extra-settings-api) mod!
 - Shield-Icon made by [Freepik](https://www.flaticon.com/authors/freepik) from [flaticon.com](https://www.flaticon.com/)
---
+++ almost-no-durability/almost-no-durability/AlmostNoDurability.cs
@@ -0,0 +1,116 @@
+﻿using HarmonyLib;
+using System;
+using System.Reflection;
+using UnityEngine;
+using System.Collections.Generic;
+
+public class AlmostNoDurability : Mod
+{
+    static JsonModInfo modInfo;
+    static Dictionary<int, int> counterCache = new Dictionary<int, int>();
+    static int modifier = 0;
+    
+
+    Harmony harmony;    
+
+    public static void Log(object message)
+    {
+        Debug.Log("[" + modInfo.name + "]: " + message.ToString());
+    }
+
+    public static void ErrorLog(object message)
+    {
+        Debug.LogError("[" + modInfo.name + "]: " + message.ToString());
+    }
+
+    public void Start()
+    {
+        modInfo = modlistEntry.jsonmodinfo;
+        harmony = new Harmony("de.felixpuetz.raftmods.almostnodurability");
+        harmony.PatchAll(Assembly.GetExecutingAssembly());
+        Log("loaded :) [v"+ modInfo.version +"]");
+    }
+
+    public void OnModUnload()
+    {
+        harmony.UnpatchAll();
+        Destroy(gameObject);
+        Log("unloaded :(");
+    }
+
+    [HarmonyPatch(typeof(Slot), "IncrementUses")]
+    public class HarmonyPatch_Equipment_UpdateEquipment
+    {
+        [HarmonyPrefix]
+        static void Prefix(ref Slot __instance, ref int amountOfUsesToAdd)
+        {
+            bool prevent = false;
+
+            if (amountOfUsesToAdd != null)
+            {
+                if (modifier > 0) {
+                    int hash = __instance.GetHashCode();
+
+                    if (counterCache.ContainsKey(hash)) {
+                        counterCache[hash] = counterCache[hash] + 1;
+                    } else {
+                        counterCache.Add(hash, 1);
+                    }
+
+                    if (counterCache[hash] < modifier) {
+                        prevent = true;
+                    } else {
+                        counterCache[hash] = 0;
+                    }
+                } else {
+                    prevent = true;
+                }
+
+                if (amountOfUsesToAdd < 0 && prevent)
+                {
+                    amountOfUsesToAdd = 0;
+                }
+            }
+        }
+    }
+
+    /***************
+    *   SETTINGS   *
+    ***************/
+    static HarmonyLib.Traverse ExtraSettingsAPI_Traverse;
+    static bool ExtraSettingsAPI_Loaded = false;
+
+    
+    public void ExtraSettingsAPI_Load()
+    {
+        modifier = ExtraSettingsAPI_GetComboboxSelectedIndex("Durability Modifier");
+    }
+
+    public void ExtraSettingsAPI_SettingsOpen()
+    {
+        if (modifier > 0) {
+            ExtraSettingsAPI_SetComboboxSelectedIndex("Durability Modifier", modifier);
+        } else {
+            ExtraSettingsAPI_SetComboboxSelectedIndex("Durability Modifier", 0);
+        }
+    }
+
+    public void ExtraSettingsAPI_SettingsClose()
+    {
+        modifier = ExtraSettingsAPI_GetComboboxSelectedIndex("Durability Modifier");
+    }
+
+    public void ExtraSettingsAPI_SetComboboxSelectedIndex(string SettingName, int value)
+    {
+        if (ExtraSettingsAPI_Loaded)
+            ExtraSettingsAPI_Traverse.Method("setComboboxSelectedIndex", new object[] { this, SettingName, value }).GetValue<int>();
+    }
+
+    public int ExtraSettingsAPI_GetComboboxSelectedIndex(string SettingName)
+    {
+        if (ExtraSettingsAPI_Loaded)
+            return ExtraSettingsAPI_Traverse.Method("getComboboxSelectedIndex", new object[] { this, SettingName }).GetValue<int>();
+        return -1;
+    }
+
+}
